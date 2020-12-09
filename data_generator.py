99107574cb98fd36b6a31944131880a41fda6b07
LIZH <lizh753159@gmail.com>
2020-12-09 09:36:25 +0800

    資料擴充.py

---
+++ data_generator.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+def parse_function(images):
+
+    images = tf.image.random_brightness(images, 0.1)
+    images = tf.image.random_saturation(images, 0.7, 1.3)
+    images = tf.image.random_contrast(images, 0.6, 1.5)
+    images = tf.image.random_flip_left_right(images)
+
+    return images
+
+
+if __name__ == '__main__':
+
+    dataset = np.load('./datasets/x_train_224.npy')
+    image_target = pd.read_csv('./datasets/train.csv')['target']
+    x_0 = []
+    x_1 = []
+    size_0 = 20000
+    size_1 = 5000
+
+    for i in range(len(image_target)):
+
+        if image_target[i] == 0:
+            x_0.append(dataset[i])
+        else:
+            x_1.append(dataset[i])
+
+    x_0 = x_0[:size_0]
+
+    while len(x_1) != size_1:
+        random_int = np.random.randint(0, len(x_1))
+        img = np.array(x_1[random_int])
+        img = parse_function(img)
+        x_1.append(img)
+
+    g_data = np.append(x_0, x_1, axis=0)
+    target = np.append(np.zeros(len(x_0)), np.ones(len(x_1)))
+    print(g_data.shape)
+    print(target.shape)
+    np.save('./datasets/g_train_224_x', g_data)
+    np.save('./datasets/g_train_224_y', target)
